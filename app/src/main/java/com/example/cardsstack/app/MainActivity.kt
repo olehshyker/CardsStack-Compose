ec89ba5cc4dac9f65434051e6c78a389793cea25
olehshyker <ms.shyker@gmail.com>
2025-04-27 16:47:44 +0300

    Using LazyColumn in cards list

--- app/src/main/java/com/example/cardsstack/app/MainActivity.kt
+++ app/src/main/java/com/example/cardsstack/app/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cardsstack.app.tickets.TicketsScreen
 import com.example.cardsstack.app.ui.theme.CardsStackComposeTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CardsStackComposeTheme {
-
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TicketsScreen(
-                        modifier = Modifier.padding(innerPadding),
-                    )
-                }
+                TicketsScreen()
             }
         }
     }
